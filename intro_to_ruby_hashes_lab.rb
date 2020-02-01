46d3d1e6dd8e3c7065e24d937c21e06ae4cce552
Harley McDowell <hmcdowell95@gmail.com>
2020-02-01 23:11:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,15 +35,11 @@ def monopoly_with_third_tier
       :reading_railroad => {},
       :pennsylvania_railroad => {},
       :b_and_o_railroad => {},
-      :shortline=>
-      {
-        "mortgage_value"=>"$800"
-      }
+      :shortline => {}
     }
   }
 end
 
 def monopoly_with_fourth_tier
-	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-
+  
 end
